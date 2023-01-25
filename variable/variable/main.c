e4a4a6b519f8e65beeb0c736fcc5c4feff240645
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2023-01-25 15:13:44 -0800

    variable basic
    integer y char

--- variable/variable/main.c
+++ variable/variable/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
+    //coment
+    //variable basic
+    char nombre[] = "Mj";
+    int edad = 27;
     printf("Hello world!\n");
+
+    printf("Me llamo  %s y tengo %d anos.\n", nombre,edad);
+    printf("Tengo %d anos y me llamo %s.  \n", edad,nombre);
+
+    edad = edad +1;
+
+    printf("El ano que viene tendre %d",edad);
+
     return 0;
 }
