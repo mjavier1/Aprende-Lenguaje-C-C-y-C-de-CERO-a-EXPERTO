064677e7ef8ca87d67b0fe223a53429ee0081d47
mjavier1 <mjaguirre526@gmail.com>
2023-01-25 19:25:47 -0400

    tipo de variable basico

---
+++ programa_c_hello/variable/tipodevariable/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero = 10;
+    float num_int=2.5r;
+    double numedec = 1.05;
+    char letras [5] = "hola";
+
+    printf("Hello world!\n");
+    return 0;
+}
