6b76a8233a4a1d8a8af42c031d82fdf308cdfc06
mjavier1 <mjaguirre526@gmail.com>
2023-01-25 20:15:34 -0400

    info basico pantalla

---
+++ programa_c_hello/variable/info/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+//variable info
+//pront salida pantalla
+{
+    double numero = 1455.585l;
+    char l = 'A';
+    printf("Mi letras %c \n",l);
+    printf("Mi numero %.12f\n", numero);
+
+    printf("Hello world!\n");
+    return 0;
+}
