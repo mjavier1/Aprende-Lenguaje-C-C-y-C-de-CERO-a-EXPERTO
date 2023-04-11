35346d5caeae59559e74e37c340839df2b15c69f
Marcos Aguirre Develop_Programing <mjaguirre5@gmail.com>
2023-04-11 19:03:27 -0400

    comentario
    programancion basuico 
    basic coment

---
+++ Comentario/coment/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    const int num = 6;
+    //valor const no cambiar
+    prinft("%d\n", num);
+
+     prinft("%d", num);
+
+    printf("Hello world!\n");
+    return 0;
+}
